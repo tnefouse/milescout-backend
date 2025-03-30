77d16bcde58cc74838d65aa2edbe76f230c4da2b
tnefouse <tnefouse@gmail.com>
2025-03-29 22:04:19 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -18,6 +18,27 @@ def all_offers():
             { "store": "apple.com", "miles_per_dollar": "2x" },
             { "store": "macys.com", "miles_per_dollar": "6x" },
             { "store": "ulta.com", "miles_per_dollar": "4x" }
+        ],
+        "American AAdvantage": [
+            { "store": "sephora.com", "miles_per_dollar": "4x" },
+            { "store": "nike.com", "miles_per_dollar": "5x" },
+            { "store": "apple.com", "miles_per_dollar": "3x" },
+            { "store": "macys.com", "miles_per_dollar": "5x" },
+            { "store": "ulta.com", "miles_per_dollar": "4x" }
+        ],
+        "Alaska Mileage Plan": [
+            { "store": "sephora.com", "miles_per_dollar": "3x" },
+            { "store": "nike.com", "miles_per_dollar": "4x" },
+            { "store": "apple.com", "miles_per_dollar": "2x" },
+            { "store": "macys.com", "miles_per_dollar": "4x" },
+            { "store": "ulta.com", "miles_per_dollar": "5x" }
+        ],
+        "Southwest Rapid Rewards": [
+            { "store": "sephora.com", "miles_per_dollar": "2x" },
+            { "store": "nike.com", "miles_per_dollar": "3x" },
+            { "store": "apple.com", "miles_per_dollar": "1x" },
+            { "store": "macys.com", "miles_per_dollar": "3x" },
+            { "store": "ulta.com", "miles_per_dollar": "4x" }
         ]
     }
     return jsonify(data)
