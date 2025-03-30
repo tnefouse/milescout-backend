cb99460eb07106dbebf561895e72aab6256c07ee
tnefouse <tnefouse@gmail.com>
2025-03-29 21:49:32 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/united')
-def united_offers():
+@app.route('/all')
+def all_offers():
     data = {
-        "united_mileageplus": [
+        "United MileagePlus": [
             { "store": "sephora.com", "miles_per_dollar": "6x" },
             { "store": "nike.com", "miles_per_dollar": "4x" },
             { "store": "apple.com", "miles_per_dollar": "3x" },
             { "store": "macys.com", "miles_per_dollar": "5x" },
             { "store": "ulta.com", "miles_per_dollar": "7x" }
+        ],
+        "Delta SkyMiles": [
+            { "store": "sephora.com", "miles_per_dollar": "5x" },
+            { "store": "nike.com", "miles_per_dollar": "6x" },
+            { "store": "apple.com", "miles_per_dollar": "2x" },
+            { "store": "macys.com", "miles_per_dollar": "6x" },
+            { "store": "ulta.com", "miles_per_dollar": "4x" }
         ]
     }
     return jsonify(data)
